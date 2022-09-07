5cacaf53c3bc7623aff76f98dbb1c5d591035ca7
Florian Isopp <florian.johannes.isopp@gmail.com>
2022-09-07 18:07:28 +0200

    basic conf json serializable

---
+++ ConfBinder/SeBuild.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+namespace ConfBinder
+{
+    public class SeBuild
+    {
+        public IConfiguration _con;
+
+        public SeBuild()
+        {
+            _con = new ConfigurationBuilder()
+                .AddJsonFile("DeviceMapper.config.json", optional: false, reloadOnChange: false)
+                .Build();
+
+            var positionOptions = new PositionOptions();
+            _con.GetSection(PositionOptions.Position).Bind(positionOptions);
+
+            Debug.WriteLine("#################");
+            Debug.WriteLine(positionOptions.Name);
+        }
+    }
+
+    public class PositionOptions
+    {
+        public const string Position = "Position";
+
+        public string Title { get; set; } = String.Empty;
+        public string Name { get; set; } = String.Empty;
+    }
+}
